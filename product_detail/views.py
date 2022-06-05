e280324b6ba5200b897c6bc1d7ec00a711bd8a24
taidao0109 <19522148@gm.uit.edu.vn>
2022-06-06 01:23:37 +0700

    product_grid, product_detail setup

--- product_detail/views.py
+++ product_detail/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
-
+from product_grid.views import getproduct
+from product_grid.models import Products, Categories,ProductImage
+from django.http import HttpResponse
 # Create your views here.
-def product_detail(request):
-    return render(request,"product_detail/product_detail.html")
+def product_detail(request,product):
+    queryset = Products.objects.prefetch_related('product_image').filter(slug=product)
+    prod_detail= getproduct(queryset)
+    imgs_count= len(prod_detail['images']) 
+    print(imgs_count)
+    if queryset:
+        return render(request,"product_detail/product_detail.html",{
+            'prod_detail' : prod_detail,
+        })
+    else:
+        return HttpResponse('Không có sản phẩm này')
